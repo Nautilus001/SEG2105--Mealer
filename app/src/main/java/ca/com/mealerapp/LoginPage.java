9f8e6e8c594459675d10bf7ad53caf9216062964
JustinBushfield74474 <71091178+JustinBushfield74474@users.noreply.github.com>
2022-09-20 21:19:31 -0400

    renamed HomePage to LoginPage, created HomePage.java, home_page.xml, aesthetic edits to LoginPage. Changed colours and layout.

---
+++ app/src/main/java/ca/com/mealerapp/LoginPage.java
@@ -0,0 +1,14 @@
+package ca.com.mealerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
