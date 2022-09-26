d84f2e9cf9fe638eacb913db5023c30cd0089fd8
JustinBushfield74474 <71091178+JustinBushfield74474@users.noreply.github.com>
2022-09-26 11:09:17 -0400

    UI changes to home_page.xml

--- app/src/main/java/ca/com/mealerapp/LoginPage.java
+++ app/src/main/java/ca/com/mealerapp/LoginPage.java
@@ -2,14 +2,30 @@ package ca.com.mealerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginPage extends AppCompatActivity {
+    private Button loginButton;
+    private Button forgotPasswordButton;
+    private Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
+        setContentView(R.layout.login_page); //Activate the login_page.xml layout
+        Button button = findViewById(R.id.login_button); //establishing login_button as a clickable button
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){ //method to be executed when login_button is clicked
+                /* Causes it to crash
+                Intent intent = new Intent("home_page");
+                startActivity(intent);
+                */
+            }
+        });
     }
 
 
